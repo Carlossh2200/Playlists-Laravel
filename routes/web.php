2b46df948d68a2e160e1782328fb0a2792a96edc
AlexDominguez18 <alejandrodominguez1823@gmail.com>
2024-07-05 14:11:56 -0600

    feat: creating model and controller for songs management

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
 
-Route::get('/',[PagesController::class,'index']);
+
+Route::get('/', fn () => view('pages.index'));
+
+Route::prefix('songs')->group(function () {
+    Route::resource('song', SongController::class);
+});
 
 Route::get('/login',[PagesController::class,'login']);
-
-Route::get('/addSong',[PagesController::class,'addSong']);
-
 Route::get('/signup',[PagesController::class,'signup']);
-
-Route::get('/managePlaylist',[PagesController::class,'managePlaylist']);
 
 
